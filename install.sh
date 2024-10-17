57c75525b91ddfb50aa6dbf9d35c90f44cd065ad
karim759 <karimullaskmk1234@gmail.com>
2024-10-17 20:26:09 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ cat <<EOL > .devcontainer/devcontainer.json
             "version": "latest"
         }
     },
-    "postStartCommand": "python3 /workspaces/Bgmi/sahil.py",
+    "postStartCommand": "python3 /workspaces/Mor/sahil.py",
     "customizations": {
         "vscode": {
             "settings": {
